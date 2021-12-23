3741fd002ce7a2551f0d16ecab715c3da9b8de8a
gevored <gersonrevoredo@hotmail.com>
2021-12-23 23:49:19 +0000

    start dashboard page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import {Flex, Button, Stack } from '@chakra-ui/react'
 import  {Input} from '../components/Form/input'
 
-export default function Home() {
+export default function SignIn() {
   return (
   <Flex w="100vw" 
     h= "100vh" 
